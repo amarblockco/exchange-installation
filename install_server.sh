7cf9b9fb491a8cd16eb6dbff36ed4eec0b275c71
amarblockco <42132866+amarblockco@users.noreply.github.com>
2019-07-01 18:01:28 +0530

    Update install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -19,18 +19,18 @@ echo +++++++++++++++++++ Apache @ Installtion  Done ++++++++++++++++++++++++++++
 #echo +++++++++++++++++++ Php7.2 @ Installtion Done +++++++++++++++++++++++++++++++++++
 #php -v
 #echo +++++++++++++++++++ Php Version @ Installtion +++++++++++++++++++++++++++++++++++
-echo +++++++++++++++++++ MongoDB @ Installtion +++++++++++++++++++++++++++++++++++
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org=4.0.3 mongodb-org-server=4.0.3 mongodb-org-shell=4.0.3 mongodb-org-mongos=4.0.3 mongodb-org-tools=4.0.3
-echo "mongodb-org hold" | sudo dpkg --set-selections
-echo "mongodb-org-server hold" | sudo dpkg --set-selections
-echo "mongodb-org-shell hold" | sudo dpkg --set-selections
-echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
-echo "mongodb-org-tools hold" | sudo dpkg --set-selections
-sudo service mongod start
-echo +++++++++++++++++++ MongoDB @ Installtion Done +++++++++++++++++++++++++++++++++++
+#echo +++++++++++++++++++ MongoDB @ Installtion +++++++++++++++++++++++++++++++++++
+#sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 9DA31620334BD75D9DCB49F368818C72E52529D4
+#echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.0.list
+#sudo apt-get update
+#sudo apt-get install -y mongodb-org=4.0.3 mongodb-org-server=4.0.3 mongodb-org-shell=4.0.3 mongodb-org-mongos=4.0.3 mongodb-org-tools=4.0.3
+#echo "mongodb-org hold" | sudo dpkg --set-selections
+#echo "mongodb-org-server hold" | sudo dpkg --set-selections
+#echo "mongodb-org-shell hold" | sudo dpkg --set-selections
+#echo "mongodb-org-mongos hold" | sudo dpkg --set-selections
+#echo "mongodb-org-tools hold" | sudo dpkg --set-selections
+#sudo service mongod start
+#echo +++++++++++++++++++ MongoDB @ Installtion Done +++++++++++++++++++++++++++++++++++
 echo +++++++++++++++++++ Nodejs @ Installtion Done +++++++++++++++++++++++++++++++++++
 sudo apt-get update
 cd ~
